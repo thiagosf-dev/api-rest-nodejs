ea592b876c43fa1201b3b8b995f4674e30e929c2
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2023-07-11 00:35:05 -0300

    ajuste no server.ts para deploy em produção

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: Number(env.PORT) }).then(() => {
+app.listen({ host: '0.0.0.0', port: Number(env.PORT) }).then(() => {
   console.log(`HTTP Server Running: http://localhost:${env.PORT}`)
 })
