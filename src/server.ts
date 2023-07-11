09acc116821fea39ba7d3a04d4ab05aebc57267f
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2023-07-11 00:42:36 -0300

    alterado texto para o app.listen

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app'
 import { env } from './env'
 
 app.listen({ host: '0.0.0.0', port: Number(env.PORT) }).then(() => {
-  console.log(`HTTP Server Running: http://localhost:${env.PORT}`)
+  console.log(`HTTP Server Running on port ${env.PORT}`)
 })
